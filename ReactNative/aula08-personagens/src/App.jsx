3113841aef05fcdc8c9f7e5dfa049a1b4da72c31
arturbazza <agencia@fonzie.com.br>
2023-12-02 09:33:57 -0300

    enviando print do resultado e testes em App.jsx

--- ReactNative/aula08-personagens/src/App.jsx
+++ ReactNative/aula08-personagens/src/App.jsx
@@ -3,8 +3,6 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
-import React from 'react';
-
 const Personagens = ({ familia }) => {
   const personagens = [
     'Jill Valentine',
